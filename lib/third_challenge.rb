9c5a0476f5cfe8cd422ae8cb0f7fc09a8bf4ef75
Sarah Paz <sarah.paz@gmail.com>
2018-12-24 18:28:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,10 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+      ]
    },
    :capulet => {
       :patriarch => {
@@ -30,7 +33,10 @@ def third_challenge
         :age => "15",
         :staus => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {:name => "Steven", :age => "30", :attitude => "confused"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
+      ]
    }
   }
 
