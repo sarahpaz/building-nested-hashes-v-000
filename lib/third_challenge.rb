8afca41fa844d0c9041dab7ceaa40553beaddc43
Sarah Paz <sarah.paz@gmail.com>
2018-12-24 18:31:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -34,8 +34,7 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
+        
       ]
    }
   }
